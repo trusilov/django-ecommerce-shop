741c2ae7a0217d78ba0a32fce4a2137282d2fc2a
trusilov <o.trusilov@gmail.com>
2021-06-10 23:30:46 +0300

    fix static root

--- shop/settings.py
+++ shop/settings.py
@@ -126,15 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = BASE_DIR / 'static'
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static_dev'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static_dev'),
+)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
